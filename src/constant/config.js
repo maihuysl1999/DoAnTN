2f7031f7cd9936aa72e24d3b7c1d846f96459846
mai huy <maihuy@mais-MacBook-Air.local>
2022-06-23 14:31:51 +0700

    feat: register

--- src/constant/config.js
+++ src/constant/config.js
@@ -1,11 +1,11 @@
-export const CORE_SERVICE_URL = "http://128.199.170.162:8090/v1";
+export const CORE_SERVICE_URL = "";
 
-export const ACCOUNT_SERVICE_URL = "https://account.v-chain.vn/v1";
+export const ACCOUNT_SERVICE_URL = "";
 
-export const TOKEN_SERVICE_URL = "https://token.v-chain.vn/v1";
+export const TOKEN_SERVICE_URL = "";
 
-export const V_STORAGE_URL = "https://storage.v-chain.vn/v1";
+export const V_STORAGE_URL = "";
 
-export const DOCS_DAPP_URL = "http://docs.v-chain.vn/apps";
+export const DOCS_DAPP_URL = "";
 
-export const IPFS_URL = "https://ipfs.v-chain.vn/ipfs";
+export const IPFS_URL = "";
